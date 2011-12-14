03b968127cf62945430b35c3c26674282892e10c
Remy D <remyd@civx.us>
2011-12-14 18:45:32 -0500

    PEP8 as best I could. Renamed version one first attempt.

--- hexicolorrainbow.py
+++ hexicolorrainbow.py
@@ -2,7 +2,7 @@
 #
 # ..author: RemyD <remyd@civx.us>
 # ..author: Ralph Bean <ralph.bean@gmail.com>
-from fabulous.color import *
+from fabulous.color import bold, bg256
 
 # Here we use a lamda to generate rgb bitspans from 0 to 255
 # Here we use color lab to generate a range of colors (in rgb format)
@@ -13,12 +13,15 @@ from fabulous.color import *
 print 'The HexiColorRainbow'
 
 step = 1
-bitspan = lambda : range(0, 256, step)
+bitspan = lambda: range(0, 256, step)
+
 
 def print_color(r, g, b):
-    print bold(bg256('  #%.2x%.2x%.2x  ' % (r, g, b), '  #%.2x%.2x%.2x  ' % (r, g, b)))
+    print bold(bg256('#%.2x%.2x%.2x' % (r, g, b), '  #%.2x%.2x%.2x  ' % (r, g, b)))
+
 
 f = open('hexcodes.txt', 'w')
+
 
 from itertools import product
 for r, g, b in product(bitspan(), bitspan(), bitspan()):
